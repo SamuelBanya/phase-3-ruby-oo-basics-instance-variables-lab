7a464fad5dda1e4173849d1fc7dd86b187ce4563
Samuel Banya <sbanya@fastmail.com>
2022-08-22 18:38:22 -0400

    Adding latest changes to assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+  # This is the 'setter' method:
+  def name=(dog_name)
+    # This is using an 'Instance Variable' since we are referring to an
+    # instance of the 'Dog' class's name
+    # Ex: Not all dogs are named 'Lassie'
+    @this_dogs_name = dog_name
+  end
 
+  # This is the 'getter' method:
+  def name
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+
+# NOTE: This is equivalent to calling: lassie.name=("Lassie")
+lassie.name = "Lassie"
+
+# This is calling the "Dog#name" method, which is the 'getter' method:
+puts lassie.name
